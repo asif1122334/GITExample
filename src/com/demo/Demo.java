81dc1be3e6fe326c9ab0ec0099a77a379bcabe66
asif <asif@gmail.com>
2024-05-13 18:03:59 +0530

    First commit...

---
+++ src/com/demo/Demo.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class Demo {
+
+ public static void main(String[] args) {
+	
+	 String hungry="yes";
+	 if(hungry=="yes") {
+		 System.out.println("eat something....");
+	 }else {
+		 System.out.println("DO work....");
+	 }
+}
+
+}
